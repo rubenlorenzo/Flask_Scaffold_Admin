61c7ca55bf78a3b40ba07d318b49d7675a95ff54
rubenlorenzo <rubenlorenzo85@yahoo.es>
2017-02-02 12:37:52 +0100

    add profile and part edit_profile

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
-from flask_user import login_required, roles_required
+from flask_user import login_required, confirm_email_required, roles_required
 from app import app, db
 from app.models import User, Role
+from sqlalchemy.orm import aliased
+
 
 #Root - route
 @app.route('/')
@@ -14,8 +16,20 @@ def home_page():
 def members_page():
     return render_template('members.html', users=db.session.query(User).all())
 
+@app.route('/members/profile/<name>')
+@login_required
+def profile(name):
+    return render_template('profile.html', user=db.session.query(User).filter(User.username == name).first())
+
+@app.route('/members/myprofile/edit')
+@login_required
+@confirm_email_required
+def edit_profile():
+    return render_template('edit_profile.html');
+
+
 #Admin_page - route
 @app.route('/admin/page')
 @roles_required('admin')
 def admin_page():
-    return render_template('admin_page.html',)
+    return render_template('admin_page.html')
