af569ac4c6aa4983679b29991ba8cef5ad1ba8bd
rubenlorenzo <rubenlorenzo85@yahoo.es>
2017-05-19 11:53:00 +0200

    initial channel options

--- app/data_visualize/views.py
+++ app/data_visualize/views.py
@@ -10,3 +10,10 @@ from . import data_visualize_admin
 @roles_required('admin')
 def channels_page():
     return render_template('admin/channels_visualize_page.html', blueprint_title="Admin", title="Data Visualize")
+
+@data_visualize_admin.route('/<channel>')
+@login_required
+@confirm_email_required
+@roles_required('admin')
+def channel_options_page(channel):
+    return render_template('admin/channel_options_page.html', blueprint_title="Admin", title="Data Visualize", subtitle=channel , channel=channel)
